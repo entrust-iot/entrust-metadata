0cf5e9d30379710875565815f26e77d4acb3f9cb
Joel Lord <jlord@macadamian.com>
2015-12-14 11:52:19 -0500

    Added /init for unique Id and tenant

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
+var uniqueId = require("libs/guid");
+
 var app = express();
 
+var ids = [];
+
+//GET on / will return server status
 app.get('/', function (req, res) {
-  console.log("Got request at /");
-  res.send('Hello World!');
+  var genericResp = "Server status: running<br>";
+  genericResp += "Current date/time: " + (new Date()).toString() + "<br>";
+  genericResp += "Hello world";
+  res.send(genericResp);
+});
+
+//GET on /init will return a unique id and tenant id
+app.get("/init", function (req, res) {
+  var data = {};
+  data.id = uniqueId(ids);
+  ids.push(data.id);
+  data.tenant = "TENANT1";
+  res.send(JSON.stringify(data));
 });
 
 var appPort = process.env.PORT || 5000;
