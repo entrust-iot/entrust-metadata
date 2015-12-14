3877bcfb197dacc8f27963e9b2e8e5683478a7c1
Joel Lord <jlord@macadamian.com>
2015-12-14 11:53:24 -0500

    Adding guid custom module

---
+++ libs/guid.js
@@ -0,0 +1,19 @@
+//Generates a GUID
+
+function generateGUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = crypto.randomBytes(1)[0]%16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+  });
+}
+
+function generateUniqueGUID(listOfIds) {
+  var uniqueId = generateGUID();
+  if (listOfIds.indexOf(uniqueId) > -1) {
+    return generateUniqueGUID();
+  } else {
+    return uniqueId;
+  }
+}
+
+module.exports = generateUniqueGUID;
