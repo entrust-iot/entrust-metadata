27a13ddad91efafe97fd06d3cbb66cf922342d8a
Joel Lord <jlord@macadamian.com>
2015-12-14 18:48:29 -0500

    Added a POST for the meta data

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var uniqueId = require("./libs/guid");
+var bodyParser = require("body-parser");
 
 var app = express();
 
 var ids = [];
+
+var db = {
+    requests: []
+};
+
+app.use(bodyParser.json())
 
 //GET on / will return server status
 app.get("/", function (req, res) {
@@ -20,6 +27,18 @@ app.get("/init", function (req, res) {
   ids.push(data.id);
   data.tenant = "TENANT1";
   res.send(JSON.stringify(data));
+});
+
+//POST for the meta data that comes from the service gateway
+app.post("/meta", function(req, res) {
+    var body = req.body;
+    //TODO Check for valid data (tenant, id, ...)
+    db.requests.push({
+        'type' : body.cmd,
+        'length': body.length,
+        'topic': body.topic
+    });
+    res.send(JSON.stringify({tenant_url: "http://blablabla"}));
 });
 
 var appPort = process.env.PORT || 5000;
